1468f9c15ae0d65062cd395555757e249d526458
auerbeatriz <biaauer03@gmail.com>
2024-12-20 23:34:28 -0300

    feat: add test file

---
+++ src/Conversor.ts
@@ -0,0 +1,6 @@
+export class Conversor {
+    static inteiro2romano(n: number): string {
+        // todo
+        return ''
+    }
+}
---
+++ test/conversao-inteiro-romano.test.ts
@@ -0,0 +1,59 @@
+import { Conversor } from "../src/Conversor"
+
+test('Conversão de Inteiro em Romano', () => {
+
+    it('Deve converter 1 em I', () => {
+        const response: string = Conversor.inteiro2romano(1)
+        expect(response).toEqual('I')
+    })
+
+    it('Deve converter 3 em III', () => {
+        const response: string = Conversor.inteiro2romano(3)
+        expect(response).toEqual('III')
+    })
+
+    it('Deve converter 4 em IV', () => {
+        const response: string = Conversor.inteiro2romano(4)
+        expect(response).toEqual('IV')
+    })
+
+    it('Deve converter 5 em V', () => {
+        const response: string = Conversor.inteiro2romano(5)
+        expect(response).toEqual('V')
+    })
+
+    it('Deve converter 6 em VI', () => {
+        const response: string = Conversor.inteiro2romano(6)
+        expect(response).toEqual('VI')
+    })
+
+    it('Deve converter 10 em X', () => {
+        const response: string = Conversor.inteiro2romano(10)
+        expect(response).toEqual('X')
+    })
+
+    it('Deve converter 20 em XX', () => {
+        const response: string = Conversor.inteiro2romano(20)
+        expect(response).toEqual('XX')
+    })
+
+    it('Deve converter 50 em L', () => {
+        const response: string = Conversor.inteiro2romano(50)
+        expect(response).toEqual('L')
+    })
+
+    it('Deve converter 100 em C', () => {
+        const response: string = Conversor.inteiro2romano(100)
+        expect(response).toEqual('C')
+    })
+
+    it('Deve converter 500 em D', () => {
+        const response: string = Conversor.inteiro2romano(500)
+        expect(response).toEqual('D')
+    })
+
+    it('Deve converter 1000 em M', () => {
+        const response: string = Conversor.inteiro2romano(1000)
+        expect(response).toEqual('M')
+    })
+})
